90e2085622772bd26a4902c8c270e372b589dc10
Ionela Pastrama <ionipas@yahoo.com>
2019-01-20 02:44:25 +0200

    create search page component in its own file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BookShelfChanger from './components/BookShelfChanger'
 import BookItem from './components/BookItem'
 import BookShelf from './components/BookShelf'
 import Header from './components/Header'
+import SearchPage from './components/SearchPage'
 
 const shelfes = [
   {"text": "Currently Reading"},
@@ -68,30 +69,15 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  hideSearchPage = () => {
+    this.setState({ showSearchPage: false })
+  }
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+            <SearchPage hideSearchPage={this.hideSearchPage} />
         ) : (
           <div className="list-books">
             <Header />
